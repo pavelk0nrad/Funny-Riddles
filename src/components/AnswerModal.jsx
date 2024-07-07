0e8b478b7cb326f1f61afe5022c93723570748ab
Pavel Konrad <115944509+pavelk0nrad@users.noreply.github.com>
2024-07-07 14:42:26 +0200

    Production Testing Environment

--- src/components/AnswerModal.jsx
+++ src/components/AnswerModal.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const AnswerModal = ({ correctAnswer, selectedOption, onClose }) => {
   const isCorrect = correctAnswer === selectedOption;
   const modalStyle = {
-    backgroundColor: isCorrect ? 'lightgreen' : 'lightcoral',
+    backgroundColor: isCorrect ? '#00c3ff' : '#FF7F00',
     padding: '20px',
     borderRadius: '10px',
     textAlign: 'center',
@@ -13,7 +13,7 @@ const AnswerModal = ({ correctAnswer, selectedOption, onClose }) => {
   return (
     <div className="modal">
       <div className="modal-content" style={modalStyle}>
-        <h2>{isCorrect ? 'Correct!' : 'Incorrect'}</h2>
+        <h2>{isCorrect ? 'Yeah! 🥳' : 'Oops...🤪'}</h2>
         <p>The correct answer is: {correctAnswer}</p>
         <button onClick={onClose}>Close</button>
       </div>
